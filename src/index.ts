2515dfb48019939e13464d555cf2318955b51596
Nikolina <nnikolinadinic@gmail.com>
2025-04-01 12:07:56 +0200

    installed required dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-console.log('1');
-console.log('2');
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const app = express()
+app.use(cors())
+app.use(morgan('tiny'))
+
+app.get('/', (req, res) => {
+    res.send('hi')
+})
+app.listen(3002, () => console.log('app started'))
