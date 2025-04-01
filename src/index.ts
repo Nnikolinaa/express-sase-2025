aca3138ed57a53f6a8aadeba5fd7c853e7f42f8f
Nikolina <nnikolinadinic@gmail.com>
2025-04-01 13:00:15 +0200

    add better structure of the project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
-import { FlightRoute } from './flight.route'
+import { FlightRoute } from './routes/flight.route'
+import { configDotenv } from 'dotenv'
+import { Timestamp } from 'typeorm'
 
 const app = express()
 app.use(cors())
@@ -9,4 +11,14 @@ app.use(morgan('tiny'))
 
 app.use('/api/flight', FlightRoute)
 
-app.listen(3002, () => console.log('app started'))
+
+app.use((req, res) => {
+    res.status(404).json({
+        message: 'NOT_FOUND',
+        Timestamp: new Date()
+    })
+})
+
+configDotenv()
+const port = process.env.SERVER_PORT || 3002
+app.listen(port, () => console.log(`app started on port ${port}`))
